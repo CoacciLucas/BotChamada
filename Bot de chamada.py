a9df65b43962f8c54c049e1ca1c5f476277ce0ec
Lucas Coacci <48459327+CoacciLucas@users.noreply.github.com>
2022-05-10 08:58:25 -0300

    Add word list for trigger

--- Bot de chamada.py
+++ Bot de chamada.py
@@ -1,21 +1,26 @@
 import speech_recognition as sr
-import pyttsx3
 import pyautogui as pg
+import pyttsx3
 
 
 audio = sr.Recognizer()
 maquina = pyttsx3.init()
 
-try:
-    with sr.Microphone() as source:
-        print('Ouvindo..')
-        voz = audio.listen(source)
-        comando = audio.recognize_google(voz, language='pt-BR')
-        comando = comando.lower()
-        if 'tina' in comando:
-            print(comando)
-            pg.write('!!!!!! PRESENTE !!!!!!')
-            pg.press('enter')
-            maquina.runAndWait()
-except Exception as e:
-    print(e)
+palavras = ['lucas', 'chamada', 'luciano']
+
+while True:
+    try:
+        with sr.Microphone() as source:
+            print('Ouvindo..')
+            voz = audio.listen(source)
+            comando = audio.recognize_google(voz, language='pt-BR')
+            comando = comando.lower()
+            matchers = [comando]
+            matching = [s for s in palavras if any(xs in s for xs in matchers)]
+            if matching:
+                print(comando)
+                pg.write('!!!!!! PRESENTE !!!!!!')
+                pg.press('enter')
+                maquina.runAndWait()
+    except Exception as e:
+        print(e)
